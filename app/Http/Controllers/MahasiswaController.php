56b2654a6123983150f9c3343c329e381c9107fe
RaphielAS <7d13ilhamnurfebrianto@gmail.com>
2021-04-14 22:32:46 +0700

    fix detail

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -67,7 +67,7 @@ class MahasiswaController extends Controller
     public function show($Nim)
     {
         //menampilkan detail data dengan menemukan/berdasarkan Nim Mahasiswa
-        $Mahasiswa = Mahasiswa::find($Nim);
+        $Mahasiswa = Mahasiswa::with('kelas')->where('nim', $Nim)->first();
         return view('mahasiswas.detail', compact('Mahasiswa'));
     }
     public function edit($Nim)
